2d7bfa28692cca01ae1db76c35b57d5fdf9b865f
humberto <huagsi@hotmail.com>
2021-06-02 18:30:03 -0500

    Practica Change

--- pratica.sql
+++ pratica.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users (id SERIAL, name VARCHAR(100), username VARCHAR(100), password VARCHAR(150));
+CREATE TABLE users_humberto (id SERIAL, name VARCHAR(100), username VARCHAR(100), password VARCHAR(150));
